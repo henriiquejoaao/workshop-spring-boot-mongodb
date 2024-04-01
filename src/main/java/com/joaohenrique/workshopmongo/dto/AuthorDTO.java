85f4457a8f7dc214906c958273b60a8ef880ec12
João Henrique <joaohenriquetrindadeguimaraes@gmail.com>
2024-04-01 17:31:46 -0300

    Projeção dos dados do autor com DTO

---
+++ src/main/java/com/joaohenrique/workshopmongo/dto/AuthorDTO.java
@@ -0,0 +1,35 @@
+package com.joaohenrique.workshopmongo.dto;
+
+import java.io.Serializable;
+
+import com.joaohenrique.workshopmongo.domain.User;
+
+public class AuthorDTO implements Serializable {
+
+  private String id;
+  private String name;
+
+  public AuthorDTO() {
+  }
+
+  public AuthorDTO(User obj) {
+    id = obj.getId();
+    name = obj.getName();
+  }
+
+  public String getId() {
+    return id;
+  }
+
+  public void setId(String id) {
+    this.id = id;
+  }
+
+  public String getName() {
+    return name;
+  }
+
+  public void setName(String name) {
+    this.name = name;
+  }
+}
