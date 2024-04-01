422ada1a16d8de1d416d41a55845be0ab605e9b5
João Henrique <joaohenriquetrindadeguimaraes@gmail.com>
2024-04-01 17:40:06 -0300

    Referenciando os posts do usuário

--- src/main/java/com/joaohenrique/workshopmongo/domain/User.java
+++ src/main/java/com/joaohenrique/workshopmongo/domain/User.java
@@ -1,8 +1,11 @@
 package com.joaohenrique.workshopmongo.domain;
 
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
 
 import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.DBRef;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 @Document(collection = "user")
@@ -12,6 +15,9 @@ public class User implements Serializable {
   private String id;
   private String name;
   private String email;
+  
+  @DBRef(lazy = true)
+  private List<Post> posts = new ArrayList<>();
   
   public User() {
   }
@@ -46,6 +52,14 @@ public class User implements Serializable {
     this.email = email;
   }
 
+  public void setPosts(List<Post> posts) {
+    this.posts = posts;
+  }
+
+  public List<Post> getPosts() {
+    return posts;
+  }
+
   @Override
   public int hashCode() {
     final int prime = 31;
@@ -71,6 +85,6 @@ public class User implements Serializable {
     return true;
   }
 
-  
+
 }
 
